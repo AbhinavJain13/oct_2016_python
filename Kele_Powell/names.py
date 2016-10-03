ed2b9e9c1d22b258e193fb3503693ea144f6855b
kelep511 <kelepowell@hotmail.com>
2016-10-03 16:43:10 -0700

    Added names assignment

---
+++ Kele_Powell/names.py
@@ -0,0 +1,29 @@
+#
+# students= [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+#
+# for dic in students:
+#     print dic['first_name'], dic['last_name']
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for group,group3 in users.items():
+    count=1
+    print group
+    for names in group3:
+        print count,"-",names['first_name'], names['last_name'],"-", len(names['first_name']+names['last_name'])
+        count+=1
