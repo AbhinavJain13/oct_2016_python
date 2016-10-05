e6566c792bdf952b630d6988b9f1ad8e47492d6b
stopitdan <danjwiegand@gmail.com>
2016-10-05 14:03:10 -0700

    updated to only necessary assignments

--- DanWiegand/Multiply.py
+++
@@ -1,7 +0,0 @@
-a = [2,4,10,16]
-def multiply(my_list):
-    for i in range(len(my_list)):
-        my_list[i] *= 5
-    print a
-
-multiply(a)
--- DanWiegand/odds.py
+++
@@ -1,7 +0,0 @@
-def oddEven():
-    for count in range(1, 2001):
-        if(count % 2 == 0):
-            print "Number is {}. This is an Even Number".format(count)
-        else:
-            print "Number is {}. This is an Odd Number".format(count)
-print oddEven()
