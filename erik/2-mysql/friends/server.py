11933659a8431c709a25959a6bc7d7f96afcdbd8
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-10-06 19:09:12 -0700

    server file

--- erik/2-mysql/friends/server.py
+++ erik/2-mysql/friends/server.py
@@ -10,7 +10,7 @@ mysql = MySQLConnector(app,'friendsdb')
 # Routes --------------------------------
 
 # /
-#Root
+# Root (default)
 @app.route('/')
 def index():
     friends = mysql.query_db("SELECT * FROM friends")
@@ -22,12 +22,13 @@ def index():
 # Create a new friend
 @app.route('/friends', methods=['POST'])
 def create():
-    query: "INSERT INTO friends(first_name,last_name,occupation,created_at,updated_at)
-    VALUES(:first_name,:last_name,:occupation,NOW(),NOW())"
-    data: {
+    query = "INSERT INTO friends(first_name,last_name,occupation,created_at,updated_at) VALUES (:first_name,:last_name,:occupation,NOW(),NOW())"
+    data = {
         "first_name":    request.form['first_name'],
         "last_name":     request.form['last_name'],
-        "occupation":    request.form['occupation']
+        "occupation":    request.form['occupation'],
+        "created_at":    request.form['created_at'],
+        "updated_at":    request.form['updated_at']
         }
     mysql.query_db(query,data)
     return redirect('/')
@@ -44,7 +45,30 @@ def show(friend_id):
     print "query data:",data
     # Execute query, passing in a parameter obj
     friends = mysql.query_db(query, data)
-    # the output loop on index expects an array of dictionaries, called all_friends
+    # pass expected parameter(s) to the page to be rendered
     return render_template('index.html', all_friends=friends)
+
+# /update_friend/<friend_id>
+# Update a friend by id
+@app.route('/update_friend/<friend_id>', methods=['POST'])
+def update(friend_id):
+    query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :id"
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation'],
+             'id': friend_id
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+
+# /unfriend/<friend_id>
+# Remove a friend by id
+@app.route('/remove_friend/<friend_id>', methods=['POST'])
+def delete(friend_id):
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    return redirect('/')
 
 app.run(debug=True)
