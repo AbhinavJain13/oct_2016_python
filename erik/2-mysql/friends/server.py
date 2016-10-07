7cdc8dfe53eef39bb9555c5bb313cec31d94979d
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-10-06 18:40:27 -0700

    friends in prog

--- erik/2-mysql/friends/server.py
+++ erik/2-mysql/friends/server.py
@@ -1,34 +1,50 @@
 
 from flask import Flask, request, redirect, render_template, session, flash
+# below: use the connection file's name without the extension!!
 from mysqlconnection import MySQLConnector
+# app it up
 app = Flask(__name__)
+# specify the db....
 mysql = MySQLConnector(app,'friendsdb')
+
+# Routes --------------------------------
+
+# /
+#Root
 @app.route('/')
 def index():
     friends = mysql.query_db("SELECT * FROM friends")
     print "many friends ",friends
     # print friends
     return render_template('index.html',all_friends=friends)
-@app.route('/friends')
-def create():
-    print request.form['first_name']
-    print request.form['last_name']
-    print request.form['occupation']
 
+# /friends
+# Create a new friend
+@app.route('/friends', methods=['POST'])
+def create():
+    query: "INSERT INTO friends(first_name,last_name,occupation,created_at,updated_at)
+    VALUES(:first_name,:last_name,:occupation,NOW(),NOW())"
+    data: {
+        "first_name":    request.form['first_name'],
+        "last_name":     request.form['last_name'],
+        "occupation":    request.form['occupation']
+        }
+    mysql.query_db(query,data)
+    return redirect('/')
+
+# /friends/<friend_id>
+# Show a single friend by id
 @app.route('/friends/<friend_id>')
 def show(friend_id):
-
-    # Write query to select specific user by id. At every point where
-    # we want to insert data, we write ":" and variable name.
+    # to insert param variable :variable_name
     query = "SELECT * FROM friends WHERE id = :specific_id"
     # Then define a dictionary with key that matches :variable_name in query.
     print "friend data: ", friend_id
     data = {'specific_id': friend_id}
     print "query data:",data
-    # Run query with inserted data.
+    # Execute query, passing in a parameter obj
     friends = mysql.query_db(query, data)
-    # Friends should be a list with a single object,
-    # so we pass the value at [0] to our template under alias one_friend.
+    # the output loop on index expects an array of dictionaries, called all_friends
     return render_template('index.html', all_friends=friends)
 
 app.run(debug=True)
