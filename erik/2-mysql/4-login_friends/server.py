91dca232ea540fe0f4d58e91ecb18adb2c70f865
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-10-10 10:04:55 -0700

    reg wip

--- erik/2-mysql/4-login_friends/server.py
+++ erik/2-mysql/4-login_friends/server.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, request, redirect, render_template, session, flash
 import md5
+import re
 from flask_bcrypt import Bcrypt
 # below: use the connection file's name without the extension!!
 from mysqlconnection import MySQLConnector
@@ -10,6 +11,18 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 # specify the db
 mysql = MySQLConnector(app,'friendsdb')
+
+
+# Functions -----------------------------------------
+
+def validate_email(email):
+    return re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+
+def validate_letters_2(string):
+    return re.match('[a-zA-Z]{2}',string)
+
+def validate_chars_8(string):
+    return re.match('/w{8}',string)
 
 # Routes --------------------------------------------
 
@@ -33,44 +46,85 @@ def index():
 
 # LOGIN -----------------------------------------------
 
-@app.route('/login',methods=['POST'])
+@app.route('/login',methods=['POST','GET'])
 def login():
     try:
         # validation
+
+# First Name - letters only, at least 2 characters and that it was submitted
+# Last Name - letters only, at least 2 characters and that it was submitted
+# Email - Valid Email format, and that it was submitted
+# Password - at least 8 characters, and that it was submitted
+# Password Confirmation - matches password
+
         # if len(request.form['email']) < 1:
         #     flash("Email cannot be empty!") # just pass a string to the flash function
         # else:
         #     flash("Success! Your email is {}".format(request.form['email']))
 
 
-        email: request.form['email']
-        errors = []
+        email= request.form['email']
         query =  '''SELECT * FROM users WHERE email= :email LIMIT 1'''
         data = {
             email: email
             }
         attempt_user = mysql.query_db(query,data)
         if bcrypt.check_password_hash(attempt_user[0]['pw_hash'], password):
-            SESSION['current_user'] = attempt_user[0]
+            session['current_user'] = attempt_user[0]
             return redirect('/')
         else:
             return redirect('/login')
     except:
 
-        # Redirect or stay on this page.
-        return render_template('/login',errors=errors)
+
+        return render_template('login.html')
 
 
 
 # REGISTRATION ________________________________________
-@app.rounte('/registration')
+@app.route('/registration',methods=['POST','GET'])
 def register():
-    # VALIDATE FORM data
 
-    errors = []
-    # IF VALID...
+    try: #to validate and register
+        # print 'REGISTRATION POST: ',request.form
+        first_name =        request.form['first_name']
+        last_name =         request.form['last_name']
+        email =             request.form['email']
+        password =          request.form['password']
+        password_confirm =  request.form['password_confirm']
 
-    # ELSE
+        # VALIDATE FORM data
+
+
+        # IF VALID...
+
+        # Encrypt password
+        pw_hash = bcrypt.generate_password_hash(password)
+        print 'PASSWORD hash: ',pw_hash
+
+        data = {
+            "first_name":   first_name,
+            "last_name" :   last_name,
+            "email":        email,
+            "password":     pw_hash
+
+        }
+
+        query = '''INSERT INTO users(first_name,last_name,email,password,created_at,updated_at)
+                   VALUES (:first_name,:last_name,:email,:password,NOW(),NOW())'''
+        # the new user's id is returned by the INSERT
+        new_user_id = mysql.query_db(query,data)
+
+        # GRAB THE COMPLETE USER now....
+        query_full = '''SELECT * from users WHERE id = :id'''
+        data_full = {
+            "id":   session['current_user_id']
+        }
+        session['current_user'] = mysql.query_db(query_full,data_full)
+        return redirect('/')
+
+    except:
+        return render_template('registration.html')
 
 
 
@@ -85,15 +139,11 @@ def create():
 
      # run validations and if they are successful
      # we can create the password hash with bcrypt
-    password = 'jeep'
-    pw_hash = bcrypt.generate_password_hash(password)
-    print 'PASSWORD hash: ',pw_hash
 
     data = {
         "first_name":       request.form['first_name'],
         "last_name":        request.form['last_name'],
         "occupation":       request.form['occupation']
-        # ,"password":      pw_hash
         }
     mysql.query_db(query,data)
     return redirect('/')
