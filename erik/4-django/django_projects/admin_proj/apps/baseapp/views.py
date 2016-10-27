38e49e60710ba9b02a2331581717d2d19db1deaa
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-10-27 08:50:52 -0700

    admin proj

---
+++ erik/4-django/django_projects/admin_proj/apps/baseapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+def show(request):
+    print('KDKDKDKDKDKDKLDKLDKLDKLFJDLKJFDLKFJLDKJKFD')
+    return render(request,'baseapp/index.html')
